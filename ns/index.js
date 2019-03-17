1be447b1b069e7f24fef43ec3f95e82c6acbc7a5
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2019-03-17 07:03:50 +0000

    refactoring ns

--- ns/index.js
+++ ns/index.js
@@ -1,26 +1,33 @@
-var path = require('path');
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const path = require('path');
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
-app.get('/', function(req, res) {
-   res.sendFile(path.join(__dirname, 'index.html'));
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.get('/:namespace', function(req, res) {
+   res.render('space', { space: req.params.namespace});
 });
 
-app.get('/2', function(req, res) {
-   res.sendFile(path.join(__dirname, 'index2.html'));
-});
+function welcome(nsp, socket, spaceName) {
+   console.log('someone connected to '+spaceName);
+   socket.emit('hi', `Welcome client '${socket.id}' to ${spaceName}!`);
+   nsp.emit('hi', `Client '${socket.id}' joined ${spaceName}!`);
+}
 
-var nsp = io.of('/my-namespace');
+const nsp = io.of('/my-namespace');
 nsp.on('connection', function(socket) {
-   console.log('someone connected');
-   nsp.emit('hi', 'Hello everyone in my-namespace!');
+   welcome(nsp, socket, '/my-namespace');
 });
 
-var nsp2 = io.of('/your-namespace');
+const nsp2 = io.of('/your-namespace');
 nsp2.on('connection', function(socket) {
-   console.log('someone connected');
-   nsp2.emit('hi', 'Hello everyone in your-namespace!');
+   welcome(nsp2, socket, '/your-namespace');
 });
 
 http.listen(3000, function() {
